7c13d077b16f33cbec30e31076458c09cfffd8f3
netrevisanto <netrevisanto@gmail.com>
2015-11-12 15:14:23 -0200

    Atualizando referencias

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-module.exports = require("./lib/i-status-linux");
-/*
+//module.exports = require("./lib/i-status-linux");
+
 var st = require("./lib/i-status-linux");
 var util = require('util');
 
@@ -8,4 +8,8 @@ st.read(function(err, succ) {
         colors: true
     }));
 });
+
+/*
+SELECT USENAME AS USERNAME, CLIENT_ADDR AS IP, WAITING AS ESPERANDO, CURRENT_QUERY AS QUERY, EXTRACT(EPOCH FROM (CURRENT_TIMESTAMP - BACKEND_START))::INTEGER AS TEMPOEXECUCAO  FROM PG_STAT_ACTIVITY;
+SELECT T1.DATNAME AS NOMEBASE, PG_DATABASE_SIZE(T1.DATNAME) AS TAMANHO FROM PG_DATABASE T1 WHERE T1.DATNAME='?', base;
 */
